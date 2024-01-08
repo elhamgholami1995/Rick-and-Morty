7c11f6085bd1c50408587be1dbf046ad67f71b17
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-01-08 23:11:23 +0330

    set  character detail

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,5 +1,38 @@
+import { Character } from "../../data/data";
+
 function CharacterDetail() {
-  return <div>CharacterDetail</div>;
+  return (
+    <div style={{ flex: 1 }}>
+      <div className="character-detail">
+        <img
+          src={Character.image}
+          alt={Character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+          <h3 className="name">
+            <span>{Character.gender === "Male" ? "👨" : "👩"}</span>
+            <span> {Character.name}</span>
+          </h3>
+          <div className="info">
+            <span
+              className={`status ${Character.status === "Dead" ? "red" : ""}`}
+            ></span>
+            <span> {Character.status} </span>
+            <span>- {Character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last Known location :</p>
+            <p>{Character.location.name}</p>
+          </div>
+          <div className="actions">
+            <button className="btn btn--primary">Add to Favorite</button>
+          </div>
+        </div>
+      </div>
+      <div className="character-episodes"></div>
+    </div>
+  );
 }
 
 export default CharacterDetail;
