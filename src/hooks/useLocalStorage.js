25ce7044975f0eeb5659e2210602403067c73f07
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-02-16 15:48:09 +0330

    Fix the problem of useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function useLocalStorage(key) {
+export default function useLocalStorage(key, initialState) {
   const [value, setValue] = useState(
     () => JSON.parse(localStorage.getItem(key)) || initialState
   );
